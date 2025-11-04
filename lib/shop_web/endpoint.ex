657f0208364ffefdfc95bc73793dae6b76c96d39
oscar <oscar.burman005@gmail.com>
2025-11-04 11:09:32 +0100

    Function plug to handle promo codes

--- lib/shop_web/endpoint.ex
+++ lib/shop_web/endpoint.ex
@@ -50,5 +50,14 @@ defmodule ShopWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug :check_promo_code
   plug ShopWeb.Router
+
+  def check_promo_code(%Plug.Conn{:params => %{"promo" => "secret-code"}} = conn, _otps) do
+    assign(conn, :promo, true)
+  end
+
+  def check_promo_code(%Plug.Conn{} = conn, _otps) do
+    assign(conn, :promo, false)
+  end
 end
